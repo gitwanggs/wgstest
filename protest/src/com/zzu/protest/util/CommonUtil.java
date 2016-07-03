72a752b1a71f583dd654d3539acbe247467a62bb
wangguishe <wanggs@aliyun.com>
2016-07-03 10:48:35 +0800

    add method getGMTDate

--- protest/src/com/zzu/protest/util/CommonUtil.java
+++ protest/src/com/zzu/protest/util/CommonUtil.java
@@ -1,7 +1,12 @@
 package com.zzu.protest.util;
 
+import java.util.Date;
+
 public class CommonUtil {
 	public static long getCurrentTimeMillis(){
 		return System.currentTimeMillis();
+	}
+	public static Date getGMTDate(){
+		return new Date();
 	}
 }
