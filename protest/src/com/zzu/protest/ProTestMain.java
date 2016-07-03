7c0c4015859c2f791a9d3b9d009c7485feec867b
wangguishe <wanggs@aliyun.com>
2016-07-03 10:11:02 +0800

    add by git2_1

--- protest/src/com/zzu/protest/ProTestMain.java
+++ protest/src/com/zzu/protest/ProTestMain.java
@@ -6,6 +6,7 @@ public class ProTestMain {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Wrold!");
 		System.out.println("Hello add by git2!");
+		System.out.println("Hello add by git2_1");
 	}
 
 }
